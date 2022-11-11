3551a1feb5afb582577059d5b77f7e6a7cebd3cd
Amen TETTEH <tkamen@outlook.com>
2022-11-11 17:51:02 +0000

    Update task.js file

--- src/js/modules/model/task.js
+++ src/js/modules/model/task.js
@@ -1,6 +1,6 @@
 class Task {
   constructor(index, description, completed = false) {
-    this.index = index;
+    this.index =index;
     this.description = description;
     this.completed = completed;
   }
@@ -11,25 +11,43 @@ class Task {
   }
 
   add() {
-    const Tasks = Task.getAll();
-    this.index = Task.count() + 1;
-    Tasks.push(this);
-    localStorage.setItem('tasks', JSON.stringify(Tasks));
+    const tasks = Task.getAll();
+    this.index=Task.count() + 1
+    tasks.push(this);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
     return this;
   }
 
   remove() {
-    const Tasks = Task.getAll();
-    Tasks.forEach((Task, index) => {
-      if (parseInt(Task.index, 10) === parseInt(this.index, 10)) {
-        Tasks.splice(index, 1);
+    const tasks = Task.getAll();
+    tasks.forEach((task, index) => {
+      if (parseInt(task.index, 10) === parseInt(this.index, 10)) {
+        tasks.splice(index, 1);
       }
     });
-    localStorage.setItem('tasks', JSON.stringify(Tasks));
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  save() {
+    const tasks = Task.getAll();
+     tasks[this.index-1].description = this.description
+    tasks[this.index-1].completed = this.completed
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  setCompleted(boolean){
+    this.completed=boolean
+    this.save()
+    return this
   }
 
   static count() {
     return this.getAll().length;
   }
+
+  static isOrdered(){
+    return localStorage.getItem('isDataOrdered');
+  }
+
 }
 export default Task;
